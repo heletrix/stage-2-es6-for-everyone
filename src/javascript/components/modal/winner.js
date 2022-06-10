e2fed2e966e8e16ae8592022508406ba7d26171d
Olena Havryliuk <olena@hellodobby.com>
2022-06-10 22:31:32 +0300

    add showing winner modal

--- src/javascript/components/modal/winner.js
+++ src/javascript/components/modal/winner.js
@@ -1,3 +1,10 @@
+import { showModal } from './modal';
+
 export function showWinnerModal(fighter) {
   // call showModal function 
+  showModal({
+    title: "The fight is over",
+    bodyElement: fighter === 'No Winner' ? "friendship won the fight!" : `${fighter.name} won the fight`,
+    onClose: () => location.reload()
+  })
 }
